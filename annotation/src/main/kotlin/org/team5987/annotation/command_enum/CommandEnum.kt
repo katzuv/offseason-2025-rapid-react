3e9ad855dda04b3c3b41f8816baf9909f25b7d54
AdarWa <adar.wasserman@gmail.com>
2025-12-03 09:14:56 +0200

    Allow annotation only on classes(enums)

--- annotation/src/main/kotlin/org/team5987/annotation/command_enum/CommandEnum.kt
+++ annotation/src/main/kotlin/org/team5987/annotation/command_enum/CommandEnum.kt
@@ -1,5 +1,5 @@
 package org.team5987.annotation.command_enum
 
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
+@Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.SOURCE)
 annotation class CommandEnum()
